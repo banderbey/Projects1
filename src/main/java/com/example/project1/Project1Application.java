38e00fde2b5a7c3d21edd53b149df2592f2f547e
David <davidnazariykrupka@triton.edu>
2021-08-19 20:52:45 +0300

    2

--- src/main/java/com/example/project1/Project1Application.java
+++ src/main/java/com/example/project1/Project1Application.java
@@ -3,8 +3,10 @@ package com.example.project1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+@EnableJpaRepositories
 public class Project1Application {
 
     public static void main(String[] args) {
--- src/main/java/com/example/project1/model/UserRepository.java
+++ src/main/java/com/example/project1/model/UserRepository.java
@@ -5,7 +5,6 @@ import com.example.project1.model.UserDAO;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 @Repository
 public interface UserRepository extends JpaRepository<UserDAO,String> {
 
